af299b9edd2adbb540bef7e08a69b0958210f3f8
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-04-28 21:34:16 +0000

    count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with :name => "Jungle", :password => "book"
+
   def show
+  @product_count = Product.count
+  @categories = Product.group(:category_id).count.length()
   end
+
 end
