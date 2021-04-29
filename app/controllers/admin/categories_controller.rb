0b94a03324f8aade8a4639565e8b63fc19eda9b0
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-04-29 16:29:44 +0000

    category page

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,29 @@
+class Admin::CategoriesController < ApplicationController
+    def index
+        @categories =  Category.all
+    end
+
+    def new
+        @categories = Category.new
+    end
+
+    def create
+      @categories = Category.new(categories_params)
+       
+        if @categories.save
+          redirect_to [:admin , :categories]
+        else
+          render 'new'
+        end
+      end
+
+    private
+        def categories_params
+        params.require(:category).permit(
+        :name,
+        :category_id,
+        )
+        end
+   
+    
+end
