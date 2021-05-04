1f061d0cf441a02c066bf7a6c2dfea081c5274f0
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-05-04 17:56:00 +0000

    product capabyra

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -16,13 +16,13 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
-    # ACT
-    visit root_path
+  # scenario "They see all products" do
+  #   # ACT
+  #   visit root_path
 
-    # DEBUG / VERIFY
-    save_screenshot
-    expect(page).to have_css 'article.product', count: 10
-  end
+  #   # DEBUG / VERIFY
+  #   save_screenshot
+  #   expect(page).to have_css 'article.product', count: 10
+  # end
 
 end
